34d465d2c542b5452c0144d1ea9e71de8dffe278
NTUMEAML <ckwang18@gmail.com>
2026-01-30 16:55:47 +0000

    Deploying to gh-pages from @ NTUMEAML/ntumeaml.github.io@4139ae3d1a2dae646b51dfc4cee0b28db99638b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
