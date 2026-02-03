a033def4eb72bf9d7af1ea9224a76dbd56c7e488
NTUMEAML <ckwang18@gmail.com>
2026-02-03 03:07:25 +0000

    Deploying to gh-pages from @ NTUMEAML/ntumeaml.github.io@a70cefc2e952075f59513db48968acb7c9ca4b6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-news",
+  },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-news",
           title: "News",
           description: "",
           section: "Navigation",
