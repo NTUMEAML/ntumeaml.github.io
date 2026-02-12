42c9b6ef0e27348f49b75f65c52e230b12d26f15
NTUMEAML <ckwang18@gmail.com>
2026-02-12 03:55:46 +0000

    Deploying to gh-pages from @ NTUMEAML/ntumeaml.github.io@fc2ca3853e07926cd471351006931e301e8b9de7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-search",
-          title: "Search",
-          description: "Google search",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/search/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
